16e4a1d40288313237207f414a1a8970021c82b0
ArmanMamyan <armmamyan@gmail.com>
2021-10-20 10:35:15 +0400

    image effects on hover
    image effects on hover using hover-effect library

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const parent = document.querySelector('.distortion')
+new hoverEffect({
+    parent,
+    intensity: 0.4,
+    image1: './im.jpeg',
+    image2: './img3.jpeg',
+    speedIn: 2,
+    displacementImage: './disMap.png'
+})
